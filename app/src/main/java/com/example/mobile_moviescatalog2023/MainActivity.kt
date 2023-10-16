26a9b38633f9a61b6638dc0402262974212cf174
KovkinAleksei <akovkin6@gmail.com>
2023-10-16 10:29:00 +0700

    Изменён экран выбора регистрации или авторизации пользователя
    *Добавлена картинка
    *Добавлено описание кинотеатра

--- app/src/main/java/com/example/mobile_moviescatalog2023/MainActivity.kt
+++ app/src/main/java/com/example/mobile_moviescatalog2023/MainActivity.kt
@@ -4,17 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -34,48 +32,125 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    RegistrationOrLoginScreen("Android")
                 }
             }
         }
     }
 }
 
+// Кнопка возврата
 @Composable
-fun Greeting(name: String) {
+fun BackButton() {
     Box(
-    ){
+        modifier = Modifier
+            .height(40.dp)
+            .width(40.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Image (
+            painter = painterResource(id = R.drawable.back_button),
+            contentDescription = null,
+            modifier = Modifier
+                .size(12.dp)
+        )
+    }
+}
+
+// Заголовок Fильмус
+@Composable
+fun FilmusHeader() {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth(1f),
+        text = stringResource(id = R.string.cinema_name),
+        textAlign = TextAlign.Center,
+        color = Accent,
+        style = TextStyle(
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+    )
+}
+
+// Картинка на экране выбора регистрации/входа
+@Composable
+fun MovieIcon() {
+    Image (
+        painter = painterResource(id = R.drawable.movie_icon),
+        contentDescription = null,
+        modifier = Modifier
+            .padding(16.dp, 35.dp, 16.dp, 0.dp)
+            .fillMaxWidth(1f),
+        alignment = Alignment.Center,
+        contentScale = ContentScale.FillWidth
+    )
+}
+
+// Экран выбора регистрации или авторизации пользователя
+@Composable
+fun RegistrationOrLoginScreen(name: String) {
+    Column {
         Box(
             modifier = Modifier
                 .height(60.dp)
                 .padding(16.dp)
         ) {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth(1f),
-                text = "Fильмус",
-                textAlign = TextAlign.Center,
-                color = Accent,
-                style = TextStyle(
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.SemiBold
-                )
-            )
-
-            Box(
-                modifier = Modifier
-                    .height(40.dp)
-                    .width(40.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Image (
-                    painter = painterResource(id = R.drawable.back_button),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(12.dp)
-                )
-            }
+            FilmusHeader()
+            BackButton()
         }
+
+        MovieIcon()
+
+        // Надпись "Погрузись в мир кино"
+        Text (
+            modifier = Modifier
+                .padding(0.dp, 35.dp, 0.dp, 0.dp)
+                .fillMaxWidth(1f),
+            text = stringResource(id = R.string.registration_slogan),
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            ),
+            textAlign = TextAlign.Center
+        )
+
+        // Описание онлайн кинотеатра
+        Text (
+            modifier = Modifier
+                .padding(16.dp, 8.dp, 16.dp, 0.dp)
+                .fillMaxWidth(1f),
+            text = stringResource(id = R.string.cinema_description),
+            style = TextStyle(
+                fontSize = 16.sp,
+                color = Color.White
+            ),
+            textAlign = TextAlign.Center
+        )
+
+        /*Button(
+            onClick = { *//*TODO*//* },
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(16.dp, 35.dp, 16.dp, 0.dp)
+        ) {
+            Text (
+                text = "Click"
+            )
+        }
+
+        Button(
+            onClick = { *//*TODO*//* },
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(16.dp, 16.dp, 16.dp, 0.dp),
+
+            ) {
+            Text (
+                text = "Click"
+            )
+        }*/
     }
 }
 
@@ -83,6 +158,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     MobileMoviesCatalog2023Theme {
-        Greeting("Android")
+        RegistrationOrLoginScreen("Android")
     }
 }
